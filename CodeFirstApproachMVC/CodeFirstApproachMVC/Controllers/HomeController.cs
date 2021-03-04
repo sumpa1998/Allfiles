4aeaa92ac8f26ac800bf66167b466abe5850d1c1
sumit pathak <sumitpathak533@gmail.com>
2021-03-04 15:34:05 +0530

    action method added

--- CodeFirstApproachMVC/CodeFirstApproachMVC/Controllers/HomeController.cs
+++ CodeFirstApproachMVC/CodeFirstApproachMVC/Controllers/HomeController.cs
@@ -12,6 +12,10 @@ namespace CodeFirstApproachMVC.Controllers
         {
             return View();
         }
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
 
         public ActionResult About()
         {
